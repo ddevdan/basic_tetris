aa3ca0931867693ed315140536ee73a8722868a3
danieldoliveira3 <dco3@cin.ufpe.br>
2021-03-16 01:15:32 -0300

    construido codigo basico javascript

---
+++ app.js
@@ -0,0 +1,26 @@
+//var x = 1 // integer
+//var y = 'one' // string
+//var z = [1, 2] // array
+////var w = true //boolean
+//////var a // undefined
+////x = 23
+//alert(x)
+//const and let => 
+//let and var => let limita a variavel ao seu bloco atual, var sobrepoe os blocos 
+// name = "daniel"
+// function showAlert() {
+//   alert('Hi ' + name)
+// }
+
+
+// showAlert()
+
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const Score = document.querySelector("#score")
+  const StartBtn = document.querySelector("#start-button")
+
+  console.log(squares)
+  const width = 10
+})
